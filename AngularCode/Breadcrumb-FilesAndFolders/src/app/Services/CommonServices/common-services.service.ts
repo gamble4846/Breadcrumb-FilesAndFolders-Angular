93bd5e524b001b480d8422571cace57042e39a70
Rohan Patel <96464493+Apurva20997@users.noreply.github.com>
2022-08-04 16:51:51 +0530

    .

--- AngularCode/Breadcrumb-FilesAndFolders/src/app/Services/CommonServices/common-services.service.ts
+++ AngularCode/Breadcrumb-FilesAndFolders/src/app/Services/CommonServices/common-services.service.ts
@@ -16,33 +16,33 @@ export class CommonServicesService {
     iconName = iconName.toLowerCase();
     switch(iconName){
       case "info":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/infoIcon.svg`;
+        return `../../../assets/svgs/infoIcon.svg`;
       case "server":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/server.svg`;
+        return `../../../assets/svgs/server.svg`;
       case "folder":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/folder.svg`;
+        return `../../../assets/svgs/folder.svg`;
       case "application/zip":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/zip.svg`;
+        return `../../../assets/svgs/zip.svg`;
       case "text/plain":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/textPlain.svg`;
+        return `../../../assets/svgs/textPlain.svg`;
       case "image/jpeg":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/jpg.svg`;
+        return `../../../assets/svgs/jpg.svg`;
       case "image/png":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/png.svg`;
+        return `../../../assets/svgs/png.svg`;
       case "application/7z":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/7z.svg`;
+        return `../../../assets/svgs/7z.svg`;
       case "application/x-zip-compressed":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/zip.svg`;
+        return `../../../assets/svgs/zip.svg`;
       case "application/rar":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/rar.svg`;
+        return `../../../assets/svgs/rar.svg`;
       case "application/pdf":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/pdf.svg`;
+        return `../../../assets/svgs/pdf.svg`;
       case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/docx.svg`;
+        return `../../../assets/svgs/docx.svg`;
       case "application/x-rar":
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/rar.svg`;
+        return `../../../assets/svgs/rar.svg`;
       default:
-        return `../../../Breadcrumb-FilesAndFolders-Release/assets/svgs/unknownFile.svg`;
+        return `../../../assets/svgs/unknownFile.svg`;
     }
   }
 }
